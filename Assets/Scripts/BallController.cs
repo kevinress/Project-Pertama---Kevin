bff52af63f7f9dd808081e71ff7dfe96888ddde8
kevinress <96115057+kevinress@users.noreply.github.com>
2022-09-01 00:05:45 +0700

    gerakin bola & padel
    nambah gerakan bola dan padel dengan input pakai physic,
    bounce dan friction padel dan tembok

---
+++ Assets/Scripts/BallController.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour
+{
+    public Vector2 speed;
+    Rigidbody2D rig;
+
+    void Start()
+    {
+        rig = GetComponent<Rigidbody2D>();
+        rig.velocity = speed;
+    }
+}
